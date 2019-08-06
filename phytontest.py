55f03eda7f0056791efe624a71f1ee4f5c772905
juanlozano <jandresl092@gmail.com>
2019-08-05 21:54:32 -0500

    upload calc

---
+++ phytontest.py
@@ -0,0 +1,28 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> 
+>>> aDv-
+SyntaxError: invalid syntax
+>>> 
+>>> print(":::BASIC CAL:::")
+:::BASIC CAL:::
+>>> print("The add is: ", a+b)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print("The add is: ", a+b)
+NameError: name 'a' is not defined
+>>> print(":::BASIC CAL:::")
+:::BASIC CAL:::
+>>> a = 5
+>>> b = 10
+>>> print("The add is: ", a+b)
+The add is:  15
+>>> print("the subs is: "a-b)
+SyntaxError: invalid syntax
+>>> print("the subs is: ",a-b)
+the subs is:  -5
+>>> 
